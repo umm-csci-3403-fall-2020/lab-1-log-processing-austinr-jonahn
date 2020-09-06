385614b57c1567c19f969acc088511d4a3aa4242
Jonah-Northwood <north305@morris.umn.edu>
2020-09-05 23:43:06 -0500

    Add pipes to convert to correct format

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
 # Extract username column from failed_login_data.txt, sort with command "sort", run sorted list though "uniq -c" to count, awk to "'username', count" > tempFile.txt, wrap_contents.sh with tempFile.txt
+
+DIR="$1"
+
+cd DIR || exit
+
+awk failed_login_data.txt '{ print "$4" }' | \
+
+sort | \
+
+uniq -c | \
+
+awk '{ print "data.addRow([\x27"$1"\x27,", $2 "]);" }' > tempFile.txt
+
+#Need to hand tempFile.txt to wrap_contents.sh
