0bfbcc8adc49347c1e984b194688cd37fc79c25a
Jonah-Northwood <north305@morris.umn.edu>
2020-09-06 18:36:07 -0500

    Update create_username_dist

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 # Extract username column from failed_login_data.txt, sort with command "sort", run sorted list though "uniq -c" to count, awk to "'username', count" > tempFile.txt, wrap_contents.sh with tempFile.txt
-
 DIR="$1"
 
-cd DIR || exit
+cd "$DIR" || exit
 
-awk failed_login_data.txt '{ print "$4" }' | \
+awk '{ print $4 }' failed_login_data.txt | \
 
 sort | \
 
 uniq -c | \
 
-awk '{ print "data.addRow([\x27"$1"\x27,", $2 "]);" }' > tempFile.txt
+awk '{ print "data.addRow([\x27"$2"\x27,", $1 "]);" }' < tempFile.txt
 
 #Need to hand tempFile.txt to wrap_contents.sh
 
-./wrap_contents.sh tempFile.txt names namesFile.txt
+cd ..
+
+./bin/wrap_contents.sh tempFile.txt username_dist namesFile.txt
