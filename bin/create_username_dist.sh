63be71e2156950b59c5287ce2ed682ee2151f214
Jonah-Northwood <north305@morris.umn.edu>
2020-09-06 22:18:30 -0500

    Finish create_username_dist.sh
    Made wrap_contents point to the correct directories

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,7 +2,7 @@
 
 # Extract username column from failed_login_data.txt, sort with command "sort", run sorted list though "uniq -c" to count, awk to "'username', count" > tempFile.txt, wrap_contents.sh with tempFile.txt
 DIR="$1"
-
+curDir=$(pwd)
 cd "$DIR" || exit
 
 awk '{ print $4 }' failed_login_data.txt | \
@@ -11,10 +11,10 @@ sort | \
 
 uniq -c | \
 
-awk '{ print "data.addRow([\x27"$2"\x27,", $1 "]);" }' < tempFile.txt
+awk '{ print "data.addRow([\x27"$2"\x27,", $1 "]);" }' > tempFile.txt
 
 #Need to hand tempFile.txt to wrap_contents.sh
 
 cd ..
 
-./bin/wrap_contents.sh tempFile.txt username_dist namesFile.txt
+"$curDir"/bin/wrap_contents.sh "$DIR"/tempFile.txt html_components/username_dist "$DIR"/namesFile.txt
