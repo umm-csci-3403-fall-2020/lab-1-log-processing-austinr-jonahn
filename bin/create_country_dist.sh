706a076a2b182259afeebbb16b61fdcc676ab657
Jonah-Northwood <north305@morris.umn.edu>
2020-09-11 00:36:06 -0500

    Fix directory errors

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -3,13 +3,12 @@
 # Extract username column from failed_login_data.txt, sort with command "sort", run sorted list though "uniq -c" to count, awk to "'username', count" > tempFile.txt, wrap_contents.sh with tempFile.txt
 DIR="$1"
 curDir=$(pwd)
-cd "$DIR" || exit
+cd log_files/"$DIR" || exit
 
 awk '{ print $5 }' failed_login_data.txt | \
 
 sort | \
 
-#join does not work at all currently
 join  ../../etc/country_IP_map.txt - | \
 
 uniq -c | \
@@ -20,4 +19,4 @@ awk '{ print "data.addRow([\x27"$3"\x27,", $1 "]);" }' > tempFile.txt
 
 cd ..
 
-"$curDir"/bin/wrap_contents.sh "$DIR"/tempFile.txt html_components/username_dist.html "$DIR"/namesFile.txt
+"$curDir"/bin/wrap_contents.sh "$DIR"/tempFile.txt "$curDir"/html_components/country_dist "$DIR"/namesFile.txt
