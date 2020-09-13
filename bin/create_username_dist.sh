72ab5f5bb72f4096641fcabd84b1d08627218951
robi1467 <robi1467@morris.umn.edu>
2020-09-13 15:26:00 -0500

    Get create_username_distribution to pass test
    was not sorting the data in the correct spot so I changed the order around and now it is working

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,21 +1,14 @@
-
-#!/usr/bin/env bas
+#!/usr/bin/env bash
 # Extract username column from failed_login_data.txt, sort with command "sort", run sorted list though "uniq -c" to count, awk to "'username', count" > tempFile.txt, wrap_contents.sh with tempFile.txt
 DIR="$1"
 curDir=$(pwd)
+
 cd "$DIR" || exit
-
-awk '{ print $4 }' failed_login_data.txt | \
-
-sort | \
-
-uniq -c | \
-
-awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);"}' > tempFile.html
+cat failed_login_data.txt| sort | awk '{print $4}' |\
+         uniq -c | awk '{print "data.addRow([\x27"$2"\x27,"$1"]);" }' > tempFile.txt
 
 #Need to hand tempFile.txt to wrap_contents.sh
-
 cd "$curDir" || exit
 
-./bin/wrap_contents.sh "$DIR"/tempFile.html /html_components/username_dist "$DIR"/username_dist.html
-rm "$DIR"/tempFile.html
+./bin/wrap_contents.sh "$DIR"/tempFile.txt /html_components/username_dist "$DIR"/username_dist.html
+rm "$DIR"/tempFile.txt
