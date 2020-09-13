5aa22f0dd819b131512bd0304cbae4287fdb05d6
robi1467 <robi1467@morris.umn.edu>
2020-09-12 21:38:27 -0500

    Made adjustments to username dist
    I think the file types were put in inncorrectly they were txt when they might need to be html

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -3,7 +3,7 @@
 # Extract username column from failed_login_data.txt, sort with command "sort", run sorted list though "uniq -c" to count, awk to "'username', count" > tempFile.txt, wrap_contents.sh with tempFile.txt
 DIR="$1"
 curDir=$(pwd)
-cd log_files/"$DIR" || exit
+cd "$DIR" || exit
 
 awk '{ print $4 }' failed_login_data.txt | \
 
@@ -11,10 +11,11 @@ sort | \
 
 uniq -c | \
 
-awk '{ print "data.addRow([\x27"$2"\x27,", $1 "]);" }' > tempFile.txt
+awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);"}' > tempFile.html
 
 #Need to hand tempFile.txt to wrap_contents.sh
 
-cd ..
+cd "$curDir" || exit
 
-"$curDir"/bin/wrap_contents.sh "$DIR"/tempFile.txt "$curDir"/html_components/username_dist "$DIR"/namesFile.txt
+./bin/wrap_contents.sh "$DIR"/tempFile.html /html_components/username_dist "$DIR"/username_dist.html
+rm "$DIR"/tempfile
