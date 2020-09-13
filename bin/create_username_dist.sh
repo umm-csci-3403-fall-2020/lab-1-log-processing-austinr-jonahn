743bac77aa808fafaff520d55ecc0de0d3d135d7
robi1467 <robi1467@morris.umn.edu>
2020-09-13 16:26:57 -0500

    Create_username_dist now passes test---actually
    So last time I ran the test for process_client_logs so I thought this passed... I was wrong, but I believe the issue before came down to accsessing failed_login_data.txt
    The wrong way. Now this is passing and the other similar files should follow suit quickly

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
-# Extract username column from failed_login_data.txt, sort with command "sort", run sorted list though "uniq -c" to count, awk to "'username', count" > tempFile.txt, wrap_contents.sh with tempFile.txt
-DIR="$1"
-curDir=$(pwd)
+# This is a Shell Script to do some funky things with the usernames of failed login data
+###Directories###
+DIR=$1
+CurDir=$(pwd) 
 
+###Sorting and extracting data
 cd "$DIR" || exit
-cat failed_login_data.txt| sort | awk '{print $4}' |\
-         uniq -c | awk '{print "data.addRow([\x27"$2"\x27,"$1"]);" }' > tempFile.txt
+cat ./*/failed_login_data.txt | awk ' {print $4} '| sort | uniq -c |\
+	awk ' { print "data.addRow([\x27"$2"\x27, "$1"]);"}' > tempUserName.txt
 
-#Need to hand tempFile.txt to wrap_contents.sh
-cd "$curDir" || exit
+# Using wrap contentssh to add footer and header to usernames
+cd "$CurDir" || exit
+./bin/wrap_contents.sh \
+	"$DIR"/tempUserName.txt \
+       	html_components/username_dist \
+	"$DIR"/username_dist.html
 
-./bin/wrap_contents.sh "$DIR"/tempFile.txt /html_components/username_dist "$DIR"/username_dist.html
-rm "$DIR"/tempFile.txt
+# remove temp file
+rm "$DIR"/tempUserName.txt
